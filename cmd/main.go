12f086bcb197106fa68acdf09b6c3f343512ed27
Andrei <andrei.kozlov.m@gmail.com>
2021-06-22 21:59:23 +0300

    DI

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,15 @@ import (
 
 	"github.com/qDes/todo-app"
 	"github.com/qDes/todo-app/pkg/handler"
+	"github.com/qDes/todo-app/pkg/repository"
+	"github.com/qDes/todo-app/pkg/service"
 )
 
 func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
-	handlers := new(handler.Handler)
 	srv := new(todo.Server)
 
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
