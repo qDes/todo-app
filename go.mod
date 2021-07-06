737e774bffcde2021f3c399789891c778acf2226
Andrei <andrei.kozlov.m@gmail.com>
2021-07-06 21:06:09 +0300

    add /sign-in

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/qDes/todo-app
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.7.2
 	github.com/go-playground/validator/v10 v10.6.1 // indirect
 	github.com/jmoiron/sqlx v1.3.4
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,18 +3,29 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/qDes/todo-app"
 	"github.com/qDes/todo-app/pkg/repository"
 )
 
-const salt = "jsdfilscgtist3243qdas"
+const (
+	salt       = "jsdfilscgtist3243qdas"
+	signingKey = "7yno9cnfgruheirbgjkadbgl4ut0"
+	tokenTTL   = 12 * time.Hour
+)
 
 type AuthService struct {
 	repo repository.Authorization
 }
 
-func NewAuthService(repo repository.Authorization) *AuthService  {
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
 	return &AuthService{repo: repo}
 }
 
@@ -22,6 +33,21 @@ func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		StandardClaims: jwt.StandardClaims{ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt: time.Now().Unix()},
+		UserId: user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
